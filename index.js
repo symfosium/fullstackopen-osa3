80a21366445d4d1bc8316c0dd4697b59b61edac9
Anton <symfosium@gmail.com>
2024-04-09 12:44:24 +0300

    3.4

--- index.js
+++ index.js
@@ -43,15 +43,22 @@ app.get('/api/info', (request, response) => {
    response.send(message)
 })
 
-//3.3
+3.3
 app.get('/api/persons/:id', (request, response) => {
    const id = Number(request.params.id);
    const person = persons.find(person => person.id === id);
    if (person) {
       response.json(person) 
    } else {
-      response.status(404).send('<h2>Page not found</h2>').end();
+      response.status(404).send('<h2>User not found</h2>').end();
    }
+})
+
+//3.4
+app.delete('/api/persons/:id', (request, response) => {
+   const id = Number(request.params.id);
+   persons = persons.filter(person => person.id !== id);
+   response.status(204).end()
 })
 
 const PORT = 3001;
